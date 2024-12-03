eac9e9643f9693137bce6472ea03ee44ff314ab5
tog <tung.chu.790693@gmail.com>
2024-12-03 14:42:38 +0700

    update href

--- tlunews/views/home/index.php
+++ tlunews/views/home/index.php
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -121,4 +119,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
->>>>>>> Stashed changes
